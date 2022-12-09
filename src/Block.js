031027dee176b06f788060088c4b0014843890ea
rex <rex.p@appwrk.com>
2022-12-09 19:12:26 +0530

    single interval for whole snake, stuck at making him turn

--- src/Block.js
+++ src/Block.js
@@ -3,85 +3,10 @@ import {
     DIRECTIONS, SPEED, RERESH_RATE_MS
 } from './Constants'
 
-function Block({ currentDirection, initialPosition }) {
-    const [position, setPosition] = useState(initialPosition);
+function Block({ blockInfo, pivotPositions, dequePivotPositionQueue }) {
 
-    const moveRight = () => {
-        setPosition(currentPosition => {
-            const { top, left } = currentPosition;
-            const newPosition = {
-                top,
-                left: left + SPEED
-            }
-            return newPosition;
-        })
-    }
-
-    const moveLeft = () => {
-        setPosition(currentPosition => {
-            const { top, left } = currentPosition;
-            const newPosition = {
-                top,
-                left: left - SPEED
-            }
-            return newPosition;
-        })
-    }
-
-    const moveUp = () => {
-        setPosition(currentPosition => {
-            const { top, left } = currentPosition;
-            const newPosition = {
-                top: top - SPEED,
-                left
-            }
-            return newPosition;
-        })
-    }
-
-    const moveDown = () => {
-        setPosition(currentPosition => {
-            const { top, left } = currentPosition;
-            const newPosition = {
-                top: top + SPEED,
-                left
-            }
-            return newPosition;
-        })
-    }
-
-    useEffect(() => {
-        /**
-         * Logic to move the snake
-         */
-        const interval = setInterval(() => {
-            switch (currentDirection) {
-                case DIRECTIONS.LEFT:
-                    moveLeft();
-                    break;
-
-                case DIRECTIONS.RIGHT:
-                    moveRight();
-                    break;
-
-                case DIRECTIONS.DOWN:
-                    moveDown();
-                    break;
-
-                case DIRECTIONS.UP:
-                    moveUp();
-                    break;
-
-                default:
-                    break;
-            }
-        }, RERESH_RATE_MS);
-
-        //Cleanup function of this hook
-        return () => {
-            clearInterval(interval);
-        }
-    }, [position, currentDirection])
+    const {index, position, direction} = blockInfo;
+    
 
     const { top, left } = position;
     const style = {
@@ -89,8 +14,8 @@ function Block({ currentDirection, initialPosition }) {
         left: `${left}px`
     }
     return (
-        <div className="block" 
-        style={style}
+        <div className="block"
+            style={style}
         >
         </div>
     );
--- src/Snake.js
+++ src/Snake.js
@@ -1,7 +1,12 @@
 import Block from './Block';
 import { useEffect, useState } from 'react';
 import {
-    DIRECTIONS, KEYCODES, INITIAL_BLOCKS_LENGTH, INITIAL_POSITION, BLOCK_DIAMETER
+    DIRECTIONS, KEYCODES,
+    INITIAL_BLOCKS_LENGTH,
+    INITIAL_POSITION,
+    BLOCK_DIAMETER,
+    DIRECTION_KEYCODE_MAP,
+    SPEED, RERESH_RATE_MS
 } from './Constants'
 
 
@@ -9,10 +14,10 @@ import {
 
 const getInitialBlocks = () => {
     let blocks = [];
-    let position = INITIAL_POSITION;
     for (let i = 0; i < INITIAL_BLOCKS_LENGTH; i++) {
         const block = {
-            position:{
+            position: {
+                index: i,
                 top: INITIAL_POSITION.top,
                 left: INITIAL_POSITION.left + (i * BLOCK_DIAMETER)
             },
@@ -25,41 +30,146 @@ const getInitialBlocks = () => {
 
 function Snake() {
     const [blocks, setBlocks] = useState(getInitialBlocks());
-    const [currentDirection, setCurrentDirection] = useState(DIRECTIONS.DOWN);
+    const [pivotPositions, setPivotPositions] = useState({});
 
-    // const handleChangeDirection = (event) => {
-    //     switch (event.keyCode) {
-    //         case KEYCODES.LEFT:
-    //             setCurrentDirection(DIRECTIONS.LEFT);
-    //             break;
-    //         case KEYCODES.RIGHT:
-    //             setCurrentDirection(DIRECTIONS.RIGHT);
-    //             break;
-    //         case KEYCODES.UP:
-    //             setCurrentDirection(DIRECTIONS.UP);
-    //             break;
-    //         case KEYCODES.DOWN:
-    //             setCurrentDirection(DIRECTIONS.DOWN);
-    //             break;
-    //         default:
-    //             break;
-    //     }
+    const moveRight = (currentPosition) => {
+        const { top, left } = currentPosition;
+        const newPosition = {
+            top,
+            left: left + SPEED
+        }
+        return newPosition;
+    }
 
-    // }
+    const moveLeft = (currentPosition) => {
 
-    // useEffect(() => {
-    //     document.addEventListener("keydown", handleChangeDirection);
-    //     return () => {
-    //         document.removeEventListener("keydown", handleChangeDirection);
-    //     }
-    // }, [])
+        const { top, left } = currentPosition;
+        const newPosition = {
+            top,
+            left: left - SPEED
+        }
+        return newPosition;
+    }
+
+    const moveUp = (currentPosition) => {
+        const { top, left } = currentPosition;
+        const newPosition = {
+            top: top - SPEED,
+            left
+        }
+        return newPosition;
+    }
+
+    const moveDown = (currentPosition) => {
+        const { top, left } = currentPosition;
+        const newPosition = {
+            top: top + SPEED,
+            left
+        }
+        return newPosition;
+    }
+
+    useEffect(() => {
+        /**
+         * Logic to move the snake
+         */
+        const interval = setInterval(() => {
+            const newBlocks = [];
+            for (let i = 0; i < blocks.length; i++) {
+                const block = { ...blocks[i] };
+                const { index, position } = block;
+
+                //Logic to change direction
+                //Find if new Position is overlapping the pivot position
+                const { top, left } = position;
+                const key = `${top}-${left}`;
+                const pivot = pivotPositions[key];
+                if (pivot) {
+                    //change the position based on the pivot position
+                    block.direction = pivot.direction;
+                    if (index === 0) {
+                        dequePivotPositionQueue(key);
+                    }
+                }
+                //Logic to move in given direction
+                switch (block.direction) {
+                    case DIRECTIONS.LEFT:
+                        block.position = moveLeft(position);
+                        break;
+
+                    case DIRECTIONS.RIGHT:
+                        block.position = moveRight(position);
+                        break;
+
+                    case DIRECTIONS.DOWN:
+                        block.position = moveDown(position);
+                        break;
+
+                    case DIRECTIONS.UP:
+                        block.position = moveUp(position);
+                        break;
+
+                    default:
+                        break;
+                }
+                newBlocks.push(block);
+            }
+            setBlocks(newBlocks);
+        }, RERESH_RATE_MS);
+
+        //Cleanup function of this hook
+        return () => {
+            clearInterval(interval);
+        }
+
+    },[blocks, pivotPositions])
+
+    //======================================================Change Direction=============================================
+
+
+    const handleChangeDirection = (event) => {
+        const snakeHeadPosition = blocks[blocks.length - 1].position;
+        const { top, left } = snakeHeadPosition;
+        const key = `${top}-${left}`;
+        const newBlocks = [...blocks];
+        newBlocks[newBlocks.length-1].direction = DIRECTION_KEYCODE_MAP[event.keyCode];
+        setBlocks(newBlocks);
+        setPivotPositions(pp => {
+            let x = { ...pp }
+            x[key] = {
+                position: snakeHeadPosition,
+                direction: DIRECTION_KEYCODE_MAP[event.keyCode]
+            };
+            return x;
+        })
+    }
+
+    useEffect(() => {
+        document.addEventListener("keydown", handleChangeDirection);
+        return () => {
+            document.removeEventListener("keydown", handleChangeDirection);
+        }
+    }, [])
 
 
 
-    console.log({ blocks })
+    console.log(JSON.stringify(blocks));
+
+    const dequePivotPositionQueue = (key) => {
+        setPivotPositions(x => {
+            delete x[key];
+            return { ...x };
+        })
+    }
+
     return <div className='snake'>
-        {blocks.map((block, i) =>
-            < Block key={i} currentDirection={block.direction} initialPosition={block.position} ></Block>
+        {blocks.map((block) =>
+            < Block
+                key={block.index}
+                blockInfo={block}
+                pivotPositions={pivotPositions}
+                dequePivotPositionQueue={dequePivotPositionQueue} >
+            </Block>
         )}
     </div >
 }
